e036e1a34106ea62e098b3b340f777c32f941f23
CurlyCre8ive <118122735+CurlyCr8tive@users.noreply.github.com>
2026-03-03 17:43:54 -0500

    Copy and use connection-os-login.png for Connection OS project image

--- client/src/components/ProjectsSection.tsx
+++ client/src/components/ProjectsSection.tsx
@@ -6,7 +6,7 @@ import { Link } from "wouter";
 import project1 from "@assets/Screenshot (213)_1763432063864.png";
 import project2 from "@assets/Screenshot (217)_1763434952523.png";
 import makeupBagImage from "@/../../attached_assets/Screenshot (229)_1763466455741.png";
-import connectionOsImage from "@assets/generated_images/Screenshot 2026-03-03 at 3.46.30 PM.png";
+import connectionOsImage from "@assets/generated_images/connection-os-login.png";
 
 const projects = [
   {
